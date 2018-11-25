1ac7ee0e5999e1080431872c4e55502144a6c386
z1995 <zhannaavanesova@yahoo.com>
2018-11-25 14:13:15 -0700

    Final Changes

--- javascript/train.js
+++ javascript/train.js
@@ -1,3 +1,4 @@
+//initialize firebase
 var config = {
     apiKey: "AIzaSyCbethiZRUaewa7ZWkaAPQ5OoS54MxrDKg",
     authDomain: "train-scheduler-ad0ff.firebaseapp.com",
@@ -10,20 +11,24 @@ var config = {
 
 var database = firebase.database();
 
+//create variables for form inputs
 var trainName = "";
 var destination = "";
 var frequency = "";
 var trainTime = "";
 
+
 $('#submit').on("click", function (event) {
     event.preventDefault();
 
+    //grabs the values the user input
     trainName = $('#train-name').val().trim();
     destination = $('#destination').val().trim();
     frequency = $('#frequency').val().trim();
 
     trainTime = $('#train-time').val().trim();
 
+    //pushes those values and stores in firebase under new-trains folder
     database.ref('/new-trains').push({
         trainName: trainName,
         destination: destination,
@@ -31,6 +36,7 @@ $('#submit').on("click", function (event) {
         trainTime: trainTime,
     });
 
+    //clears input box 
     $('#train-name').val('');
     $('#destination').val('');
     $('#frequency').val('');
@@ -43,11 +49,14 @@ database.ref('/new-trains').on("child_added", function(snapshot) {
 
     console.log(sv.trainName, sv.destination, sv.trainTime, sv.frequency);
 
+    //variables for when a new train is added
     var newName = sv.trainName;
     var newDestination = sv.destination;
     var newFrequency = parseInt(sv.frequency);
     var firstTime = sv.trainTime;
 
+    //using moment.js for time 
+    //subtract 1 year to avoid negatives
     var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "year");
     var currentTime = moment();
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
@@ -55,6 +64,7 @@ database.ref('/new-trains').on("child_added", function(snapshot) {
     var minutesAway = newFrequency - tRemainder;
     var nextTrain = moment().add(minutesAway, "minutes").format("HH:mm");
 
+    //add the new train to the current table by adding a new table row
     var newRow = $('<tr>').append(
         $('<td>').text(newName),
         $('<td>').text(newDestination),
